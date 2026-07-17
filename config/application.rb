21094ebfcf118e788cd14ab7b63bbedc5b1833c6
Jonathan Lyons <jrl08d@icloud.com>
2026-07-17 01:09:35 -0400

    set test framework to be rspec

--- config/application.rb
+++ config/application.rb
@@ -35,5 +35,6 @@ module MyWeatherApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators.test_framework :rspec
   end
 end
